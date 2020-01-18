018d6b0ade66e6403e4a3ca109d8ec8e7b735f24
Kelvin <kelvin8_wang@qq.com>
2020-01-18 17:15:30 +0800

    > 新增了操作cookie的练习 > parse相关

--- src/data.py
+++ src/data.py
@@ -1,5 +1,24 @@
 import urllib.request
+import http.cookiejar
+from urllib.parse import urlencode
+from urllib.parse import parse_qs
+from urllib.parse import quote
+filename = '/work2/baidu.cookie'
 
-response = urllib.request.urlopen('http://www.baidu.com')
-print(response.read().decode('utf-8'))
+cookie = http.cookiejar.LWPCookieJar(filename)
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+response = opener.open('http://www.baidu.com')
+cookie.save(ignore_discard=True, ignore_expires=True)
+for item in cookie:
+    print(item.name, ' = ', item.value)
 
+
+data = urlencode({"name":"中国" , "user":"wang"})
+print(data)
+
+obj = parse_qs(data)
+print(obj['name'][0])
+
+
+print(quote('中国'))
